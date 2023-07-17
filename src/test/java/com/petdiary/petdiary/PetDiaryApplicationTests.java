62d8484930ed399ac988c2efb0dcd029882ade1f
khsFirefox <rlagnltn135@smtown.com>
2023-07-17 18:25:05 +0900

    project init

---
+++ src/test/java/com/petdiary/petdiary/PetDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petdiary.petdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetDiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
