d8703fe96fa0614941b1ebde24ef58898899eec7
khsFirefox <rlagnltn135@smtown.com>
2023-10-24 14:57:01 +0900

    swagger 설정

--- servers/auth-api/src/main/java/com/petdiary/config/WebMvcConfig.java
+++ servers/auth-api/src/main/java/com/petdiary/config/WebMvcConfig.java
@@ -2,16 +2,23 @@ package com.petdiary.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.petdiary.interceptor.LoggingInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+
 @Configuration
+@RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
+    private final Environment environment;
+
     @Bean
     public LoggingInterceptor getLoggingInterceptor() {
         return new LoggingInterceptor();
@@ -40,8 +47,20 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/static/swagger-ui.html");
-        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        // server env profiles
+        if (isProfileActive("prod", "dev")) {
+            registry.addResourceHandler("/static/swagger-ui/openapi3.yaml").addResourceLocations("classpath:/static/swagger-ui/openapi3.yaml");
+        }
+        // local env profiles
+        else {
+            registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/static/swagger-ui.html");
+            registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        }
         WebMvcConfigurer.super.addResourceHandlers(registry);
+    }
+
+    private boolean isProfileActive(String... profiles) {
+        return Arrays.stream(environment.getActiveProfiles())
+                .anyMatch(activeProfile -> Arrays.asList(profiles).contains(activeProfile));
     }
 }
